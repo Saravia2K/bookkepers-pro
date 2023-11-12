a966bef8ea24796cf8ba177696363ec9c167dabe
Diego Saravia <diego.saraviazaldana@gmail.com>
2023-11-11 18:10:52 -0600

    feat: new icons

---
+++ src/assets/icons/Diamonds.tsx
@@ -0,0 +1,52 @@
+import { SVGProps } from "react";
+
+const DiamondIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    id="a8680b01-6599-42b8-8770-d69cf82e5476"
+    data-name="Capa 1"
+    viewBox="0 0 150 150"
+    {...props}
+  >
+    <defs>
+      <style>{".e91a0e68-35a1-4da7-9435-477800e30910{fill:#233d4d}"}</style>
+    </defs>
+    <rect
+      width={49.16}
+      height={49.16}
+      x={50.42}
+      y={8.32}
+      className="e91a0e68-35a1-4da7-9435-477800e30910"
+      rx={4.49}
+      transform="rotate(-45 75 32.904)"
+    />
+    <rect
+      width={49.16}
+      height={49.16}
+      x={50.42}
+      y={92.52}
+      className="e91a0e68-35a1-4da7-9435-477800e30910"
+      rx={4.49}
+      transform="rotate(-45 75.002 117.093)"
+    />
+    <rect
+      width={49.16}
+      height={49.16}
+      x={92.52}
+      y={50.42}
+      className="e91a0e68-35a1-4da7-9435-477800e30910"
+      rx={4.49}
+      transform="rotate(-135 117.1 74.997)"
+    />
+    <rect
+      width={49.16}
+      height={49.16}
+      x={8.32}
+      y={50.42}
+      className="e91a0e68-35a1-4da7-9435-477800e30910"
+      rx={4.49}
+      transform="rotate(-135 32.9 75.002)"
+    />
+  </svg>
+);
+export default DiamondIcon;
---
+++ src/assets/icons/Flag.tsx
@@ -0,0 +1,18 @@
+import { SVGProps } from "react";
+
+const FlagIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    data-name="Capa 1"
+    viewBox="0 0 150 150"
+    {...props}
+  >
+    <path
+      d="M110.82 24.28C86.33 25.43 57.14 0 34.29 0S0 14.64 0 14.64V150h20.57v-46.85a90.3 90.3 0 0 1 17.08-1.7c20.82 0 54.19 25.43 74.39 25.43s38-12.43 38-12.43V9s-14.73 14.12-39.22 15.28Z"
+      style={{
+        fill: "#233d4d",
+      }}
+    />
+  </svg>
+);
+export default FlagIcon;
