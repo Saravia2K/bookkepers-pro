41b106209cd1f674b279d5e21bee4e1397527198
Diego Saravia <diego.saraviazaldana@gmail.com>
2023-11-11 18:30:34 -0600

    feat: adding /consulting route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import { Routes as RoutesWrapper, Route } from "react-router-dom";
 import IndexPage from "./pages/Index";
 import ServicesPage from "./pages/Services";
+import ConsultingPage from "./pages/Consulting";
 
 export default function Routes() {
   return (
     <RoutesWrapper>
       <Route path="/" element={<IndexPage />} />
       <Route path="/services" element={<ServicesPage />} />
+      <Route path="/consulting" element={<ConsultingPage />} />
     </RoutesWrapper>
   );
 }
