ca4e2f74322c42051844f7b116b9fd75992a770a
Diego Saravia <diego.saraviazaldana@gmail.com>
2023-11-11 18:11:15 -0600

    feat: services route added to router

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import { Routes as RoutesWrapper, Route } from "react-router-dom";
 import IndexPage from "./pages/Index";
+import ServicesPage from "./pages/Services";
 
 export default function Routes() {
   return (
     <RoutesWrapper>
       <Route path="/" element={<IndexPage />} />
+      <Route path="/services" element={<ServicesPage />} />
     </RoutesWrapper>
   );
 }
