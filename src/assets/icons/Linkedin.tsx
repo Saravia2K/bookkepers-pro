1b21a973e2e00b3d3dfe9d44e3e529ea111fb146
Diego Saravia <diego.saraviazaldana@gmail.com>
2023-11-12 21:09:15 -0600

    feat: new icon added

---
+++ src/assets/icons/Linkedin.tsx
@@ -0,0 +1,26 @@
+import { SVGProps } from "react";
+
+const LinkedinIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    id="Capa_1"
+    data-name="Capa 1"
+    viewBox="0 0 150 150"
+    {...props}
+  >
+    <defs>
+      <style>{".cls-2{fill:#233d4d}"}</style>
+    </defs>
+    <path
+      d="M0 0h150v150H0z"
+      style={{
+        fill: "#fe7f2d",
+      }}
+    />
+    <path
+      d="M44.5 54.56a9.75 9.75 0 0 1-9.5-10A10.08 10.08 0 0 1 44.46 35c5.17-.08 10 4.56 10.1 9.63a10 10 0 0 1-10.06 9.93ZM36.29 61.66h16.72V115H36.29zM96.28 60.68c-6.94-.23-12.52 3.15-15.92 5.94v-5H63.64V115h16.72V84c0-3 2.63-9.14 9.23-9.14s8.69 4.67 8.69 10.5V115H115V79.36c0-12.55-7-18.29-18.72-18.68Z"
+      className="cls-2"
+    />
+  </svg>
+);
+export default LinkedinIcon;
