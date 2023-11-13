e3751f89ae0b5267eb0c807ba5adef1c5e5b2d5a
Diego Saravia <diego.saraviazaldana@gmail.com>
2023-11-12 21:10:20 -0600

    feat: reverse on responsive grid

--- src/components/TwoBoxesContainer/index.tsx
+++ src/components/TwoBoxesContainer/index.tsx
@@ -1,11 +1,19 @@
 import { ReactElement } from "react";
 import styles from "./styles.module.scss";
 
-export default function TwoBoxesContainer({ box1, box2 }: Props) {
+export default function TwoBoxesContainer({
+  box1,
+  box2,
+  reverseOnResponsive,
+}: Props) {
   const boxes = [box1, box2];
 
   return (
-    <section className={styles["two-boxes-container"]}>
+    <section
+      className={`${styles["two-boxes-container"]} ${
+        reverseOnResponsive ? styles.reverse : ""
+      }`}
+    >
       {boxes.map((b, i) => (
         <div
           key={i}
@@ -24,6 +32,7 @@ export default function TwoBoxesContainer({ box1, box2 }: Props) {
 type Props = {
   box1?: Box;
   box2?: Box;
+  reverseOnResponsive?: boolean;
 };
 
 type Box = {
