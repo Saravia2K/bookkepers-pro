8785d1ee895f7049b22fd5ae0bdb403209fbf194
Diego Saravia <diego.saraviazaldana@gmail.com>
2023-11-27 08:05:40 -0600

    feat: react-toastify setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 import Routes from "./routes.tsx";
 import Navbar from "./components/Navbar/index.tsx";
 import Footer from "./components/Footer/index.tsx";
 
 import "./assets/sass/global.scss";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -16,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Routes />
         <Footer />
       </main>
+      <ToastContainer hideProgressBar limit={3} />
     </BrowserRouter>
   </React.StrictMode>
 );
